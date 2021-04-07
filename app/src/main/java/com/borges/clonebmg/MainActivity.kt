3a26b143ce158b83c623f35035c58db90c3c7050
João Victor Borges <borges.jvdo@gmail.com>
2021-04-07 17:40:48 -0300

    Finalizando clone da interface

--- app/src/main/java/com/borges/clonebmg/MainActivity.kt
+++ app/src/main/java/com/borges/clonebmg/MainActivity.kt
@@ -1,26 +1,63 @@
 package com.borges.clonebmg
 
+
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
 import androidx.core.view.isVisible
+import com.borges.clonebmg.R.*
 
 
 class MainActivity : AppCompatActivity() {
     var textViewEsconder: TextView? = null
     var textViewSaldo: TextView? = null
+    var imageView: ImageView? = null
+    var toolbar_iconmenu: ImageView? = null
+    var toolbar_iconperfil: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
 
-        textViewEsconder = findViewById(R.id.info_text_toqueesconder)
-        textViewSaldo = findViewById(R.id.info_text_saldo)
+        //Menu da Toolbar
+        toolbar_iconmenu = findViewById(id.toolbar_icon_menu)
+        toolbar_iconperfil = findViewById(id.toolbar_icon_perfil)
+
+        //Menu do CardView de Saldo
+        textViewEsconder = findViewById(id.info_text_toqueesconder)
+        textViewSaldo = findViewById(id.info_text_saldo)
+        imageView = findViewById(id.icon_refresh_saldo)
 
         textViewEsconder?.setOnClickListener {
             esconderSaldo()
+        }
+        imageView?.setOnClickListener {
+            val valorinicial = 1
+            val valorrefresh = 500
+            if(textViewSaldo?.hint == "R$ 500,00"){
+                hintDelay(valorinicial)
+            } else {
+                hintDelay(valorrefresh)
+            }
+        }
+        toolBarActions()
+    }
+
+    private fun hintDelay(i: Int) {
+        textViewSaldo?.hint = "R$ $i,00"
+    }
+
+    private fun toolBarActions() {
+
+        toolbar_iconmenu?.setOnClickListener {
+            Toast.makeText(this, "Abrir menu lateral", Toast.LENGTH_LONG).show()
+        }
+
+        toolbar_iconperfil?.setOnClickListener {
+            Toast.makeText(this, "Abrir Perfil", Toast.LENGTH_LONG).show()
         }
     }
 
